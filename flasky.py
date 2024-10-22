725c91ecd19a9df41e9404a2ccd2e7286d6581c7
checkking <checkking@foxmail.com>
2024-10-22 16:10:30 +0800

    init 8b

--- flasky.py
+++ flasky.py
@@ -1,11 +1,9 @@
 import os
 import click
-from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
